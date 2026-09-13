acc9b5e945d4e4507e9340771e9d4f4d94fe5c0b
lgfire07 <goravsidhu550@gmail.com>
2026-09-13 09:18:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands 
 
+intents = discord.Intents.default()
+intents.message_content = True 
+
+bot = commands.Bot(command_prefix="!", intents=intents) 
+
+@bot.event
+async def on_ready():
+print(f'Logged in as {bot.user.name}') 
+
+@bot.command()
+async def ticket(ctx):
+await ctx.send("Ticket system is active! Click below to create a ticket.") 
+
+
+bot.run("MTU0NTYyODI5NDcyMzAxODgzMg.GicVJ1.S1KwqJJEfGjOz5OHu8Tt69-0JATtE2PzHSabzA")
